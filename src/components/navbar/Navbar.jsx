a04e84ccfae502cace3d70c27d1705b4a61f3939
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-06-24 23:59:44 +0530

    new

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 // import { Link } from "react-router-dom";
 import "./Navbar.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Navbar = () => {
-  const [active, setActive] = useState(true);
+  const [active, setActive] = useState(false);
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+  const currentUser = {
+    id: 1,
+    usrname: "John Doe",
+    isSeller: true,
+  };
   return (
     <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
@@ -18,11 +32,31 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user">
+              <img
+                src="https://media.istockphoto.com/id/1404932505/photo/gold-lowercase-letter-s.webp?s=1024x1024&w=is&k=20&c=Yt0GAWwfG_2blYnCY5fJENFCNaapJ4eLtbdzif0B3OQ="
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              <div className="options">
+                {currentUser?.isSeller && (
+                  <>
+                    <span>Gigs</span>
+                    <span>Add New Gigs</span>
+                  </>
+                )}
+                <span>Orders</span>
+                <span>Messages</span>
+                <span>Log out</span>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-      { active && (
+      {active && (
         <>
           <hr />
           <div className="menu">
